f0d325ceb7b5bdc9e2c6f4f6908a7c73d9dd5aa4
kainatAmjad1 <159706456+kainatAmjad1@users.noreply.github.com>
2024-10-17 22:40:32 +0500

    Add files via upload

---
+++ financerepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using projectnew.Data;  // Assuming ApplicationDbContext is defined in projectnew.Data
+
+namespace projectnew.Models
+{
+    public class financerepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public financerepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Addfin(finance f)
+        {
+            _context.finance.Add(f);
+            _context.SaveChanges();
+        }
+
+        // Method to get all finances
+        public List<finance> GetFinances()
+        {
+            return _context.finance.ToList();
+        }
+        public void UpdateFinance(finance updatedFinance)
+        {
+            var existingFinance = _context.finance.Find(updatedFinance.Id);
+            if (existingFinance != null)
+            {
+                existingFinance.category = updatedFinance.category;
+                existingFinance.amount = updatedFinance.amount;
+                existingFinance.expenseDate = updatedFinance.expenseDate;
+                existingFinance.description = updatedFinance.description;
+                _context.SaveChanges();
+            }
+        }
+
+        public void DeleteFinance(finance financeToDelete)
+        {
+            _context.finance.Remove(financeToDelete);
+            _context.SaveChanges();
+        }
+
+        public void DeleteFinance(int id)
+        {
+            var financeToDelete = _context.finance.Find(id);
+            if (financeToDelete != null)
+            {
+                _context.finance.Remove(financeToDelete);
+                _context.SaveChanges();
+            }
+        }
+
+        public void UpdateAll() { }
+        public void UpdatefinanceByName(finance f) { }
+    }
+}
+
+
