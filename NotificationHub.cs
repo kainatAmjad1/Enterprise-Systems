782d8d566fb878f548a034481e32444590b94c24
kainatAmjad1 <159706456+kainatAmjad1@users.noreply.github.com>
2024-10-17 22:39:36 +0500

    Add files via upload

---
+++ NotificationHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace projectnew.hub
+{
+    public class NotificationHub : Hub
+    {
+        // Send message to all connected clients
+        public async Task SendNotification(string message)
+        {
+            await Clients.All.SendAsync("ReceiveNotification", message);
+        }
+    }
+}
+
