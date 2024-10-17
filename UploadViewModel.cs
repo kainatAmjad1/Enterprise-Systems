664dbe49672225164cae1fd58422522192059da4
kainatAmjad1 <159706456+kainatAmjad1@users.noreply.github.com>
2024-10-17 22:41:34 +0500

    Add files via upload

---
+++ UploadViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectName.Models
+{
+    public class UploadViewModel
+    {
+        [Required(ErrorMessage = "Please select a file.")]
+        [Display(Name = "File")]
+        public IFormFile File { get; set; }
+    
+    }
+}
