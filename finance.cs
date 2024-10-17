90c381afa879b3558904dfcf1b692a8dbf9d5393
kainatAmjad1 <159706456+kainatAmjad1@users.noreply.github.com>
2024-10-17 22:40:09 +0500

    Add files via upload

---
+++ finance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projectnew.Models
+{
+    [Table("finance")]
+    
+        public class finance
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public string category { get; set; }
+
+            [Required]
+            public decimal amount { get; set; }
+
+            [Required]
+            public DateTime expenseDate { get; set; }
+
+            [Required]
+            public string description { get; set; }
+        }
+    }
