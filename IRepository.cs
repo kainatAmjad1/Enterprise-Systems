4519a1fa176e71111a7dd2a576b42048b5e4d396
kainatAmjad1 <159706456+kainatAmjad1@users.noreply.github.com>
2024-10-17 22:41:16 +0500

    Add files via upload

---
+++ IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+//namespace projectnew.Models
+//{
+//    public interface IRepository<TEntity>
+//    {
+//        Task Add(TEntity entity);
+//        Task Delete(int id);
+//        Task<TEntity> GetById(int id);
+//        Task<List<TEntity>> GetAll();
+//        //void Update(TEntity entity);
+//        void UpdateById(int id, TEntity updatedEntity);
+//    }
+//}
+namespace projectnew.Models
+{
+    public interface IRepository<TEntity>
+    {
+        void Add(TEntity entity);
+        void DeleteById(int id);
+        void UpdateById(int id, TEntity updatedEntity);
+        TEntity FindById(int id);
+        IEnumerable<TEntity> GetAll();
+    }
+}
